6d1b4552a4fd970c375af82cf441b4d2b77b66bf
Michael Ying <mying@0587424486.wireless.umich.net>
2019-11-19 22:59:44 -0500

    tuned auto-drift, addded braking functionality during countersteer

--- HMC_dev/HMC_dev.ino
+++ HMC_dev/HMC_dev.ino
@@ -47,6 +47,8 @@ float yr_err_total;
 bool yrc_active;
 bool yrc_active_k1;
 int yrc_int_active;
+float yr_limit = 0.9;
+bool yr_limit_active;
 
 float kp = 200;
 float ki = 1100;
@@ -98,13 +100,14 @@ void loop() {
 
 
 
-    /* Get a new sensor event */ 
+  /* Get a new sensor event */ 
   sensors_event_t event; 
   gyro.getEvent(&event);
 
   /* Display the results (speed is measured in rad/s) */
 
   yr = event.gyro.z;
+  yr = yr-0.01;
   //Serial.print("X: "); Serial.print(event.gyro.x); Serial.print("  ");
   //Serial.print("Y: "); Serial.print(event.gyro.y); Serial.print("  ");
   //Serial.print("Z: "); Serial.print(yr); Serial.print("  ");
@@ -149,8 +152,8 @@ void loop() {
 
 
   //-----------------------------------------------------------------B I G   B O I---------------------------------------------------------------
-
-
+  Serial.print(yr);
+  Serial.print("     ");
 
   propServoValue_in = readSensor(3, 2, propServoPin_in);
   
@@ -175,29 +178,44 @@ void loop() {
   }
 
 
+  //activate yaw rate control when trying to go straight (steady forward motion or countersteer maneuver)
+  yr_limit_active = false;
   if (yr_targ == 0){
     yrc_active = true;
 
-    //activate integral term if heading straight
-    if (abs(yr) < 0.2){
+    //activate integral term if in steady forward motion
+    if (abs(yr) < 0.28){
       yrc_int_active = 1;
     }
     else{
       yrc_int_active = 0;
     }
 
+    //brake if performing severe countersteer movement (see impeller motor control logic)
+    Serial.print(abs(yr));
+    Serial.print("      ");
+    Serial.print(yr_limit);
+    Serial.print("     ");
+    if (abs(yr) > yr_limit){
+      yr_limit_active = true;
+    }
+
       
-    //reset integral
+    //reset integral if initializing yrc
     if (yrc_active_k1 == false){
       yr_err_total = 0;
     }
-    
+
+
+
+    //compute  errors
     yr_err = yr - yr_targ;
     yr_err_total += yr_err * ts/1000;
 
 
 
     //Output signal
+    //direction logic
     if (dir == LOW){
       //moving forwards
       propServoValue_out = 1320 + kp*yr_err + yrc_int_active*ki*yr_err_total + kd*(yr_err - yr_err_k1)/(ts/1000);
@@ -207,10 +225,13 @@ void loop() {
       propServoValue_out = 1320 - kp*yr_err - yrc_int_active*ki*yr_err_total - kd*(yr_err - yr_err_k1)/(ts/1000);
 
     }
+
     
   }
+  
+  //deactivate yrc if user requests a turn
   else {
-    //turn requested. Control using joystick
+ 
     yrc_active = false;
     propServoValue_out = 0.436*propServoValue_in + 730.25;
     
@@ -233,10 +254,10 @@ void loop() {
       propServoValue_out = 650; 
     }
 
+    //deadband not needed here since yrc is used if trying to fly straight
 
 
-
-   
+  //update variables
   yr_err_k1 = yr_err;
   yrc_active_k1 = yrc_active;
 
@@ -254,7 +275,7 @@ void loop() {
   else{
     impMotorValue_out = -0.6235*propMotorValue_in + 809.89;
   }
-  
+
   
   //control signal bounds
   if (impMotorValue_out < 0){
@@ -264,11 +285,20 @@ void loop() {
     impMotorValue_out = 255;
   }
 
+  Serial.print(yr_limit_active);
+  Serial.print("     ");
+  //brake if countersteering
+  if (yr_limit_active == true){
+    impMotorValue_out = 0;
+  }
+
   //deadband around neutral
   if (abs(propMotorValue_in - 1288) < 50){
     impMotorValue_out = 0;
   }
 
+  
+  
   //Output
   
   digitalWrite(dirPin_out, dir);
